a902835c98fffcb868227811b14b1a2b83b62165
cibertp <cibertp462@gmail.com>
2017-03-07 01:35:05 -0500

    un breve avance
    hasta el desligue del menu..

---
+++ js/menu.js
@@ -0,0 +1,6 @@
+var  btnMenu =  document.getElementById("btn-menu");
+var nav = document.getElementById("nav");
+
+btnMenu.addEventListener('click',function()){
+    nav.clasList.toggle('mostrar');/* esto lo crearemos en el css en la parete de la cabeseara*/
+}
